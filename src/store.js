0b1f32ab3b1a599dfed5ac93f72f92f5620599d0
carlos23fb <jcflobe@gmail.com>
2021-10-03 12:41:42 -0600

    Setup logic for updateEvent()

--- src/store.js
+++ src/store.js
@@ -2,36 +2,44 @@ import {reactive} from 'vue'
 import {seedData} from './seed.js'
 
 export const store = {
-    state:{
+    state: {
         data: reactive(seedData)
     },
-    getActiveDay(){
+    getActiveDay() {
         return this.state.data.find((day) => day.active)
     },
-    setActiveDay(dayId){
+    setActiveDay(dayId) {
         this.state.data.map((dayObj) => {
-            dayObj.id === dayId ? dayObj.active = true : dayObj.active 
-            =false;
+            dayObj.id === dayId ? dayObj.active = true : dayObj.active
+                = false;
         });
     },
-    submitEvent(eventDetails){
+    submitEvent(eventDetails) {
         const activeDay = this.getActiveDay();
         activeDay.events.push({"details": eventDetails, "edit": false})
     },
 
     // • Filter state data , based on day.id , to get the day that the event is being edited.
     // • Filter the events array of the targeted day, based on event.details , to get the targeted event.
-    editEvent(dayId, eventDetails){
+    editEvent(dayId, eventDetails) {
         this.resetEditOfAllEvents()
-        const dayObj = this.state.data.find((day) => day.id === dayId);
-        const eventObj = dayObj.events.find((event) => event.details === eventDetails);
+        const eventObj = this.getEventObj(dayId, eventDetails);
         eventObj.edit = true;
     },
-    resetEditOfAllEvents(){
-        this.state.data.map((dayObj) =>{
-            dayObj.events.map(event =>{
+    resetEditOfAllEvents() {
+        this.state.data.map((dayObj) => {
+            dayObj.events.map(event => {
                 event.edit = false;
             })
         })
+    },
+    updateEvent(dayId, originalEventDetails, newEventDetails) {
+        const eventObj = this.getEventObj(dayId, originalEventDetails);
+        eventObj.details = newEventDetails;
+        eventObj.edit = false;
+    },
+    getEventObj(dayId, eventDetails) {
+        const dayObj = this.state.data.find(day => day.id === dayId);
+        return dayObj.events.find(event => event.details === eventDetails)
     }
 }
