7ed61c9b4501f26fb9ad2596dd8192c6681adb98
carlos23fb <jcflobe@gmail.com>
2021-10-03 11:01:37 -0600

    setup logic for edit events with: editEvent resetEditOfAllEvents in store.js

--- src/store.js
+++ src/store.js
@@ -17,5 +17,21 @@ export const store = {
     submitEvent(eventDetails){
         const activeDay = this.getActiveDay();
         activeDay.events.push({"details": eventDetails, "edit": false})
+    },
+
+    // • Filter state data , based on day.id , to get the day that the event is being edited.
+    // • Filter the events array of the targeted day, based on event.details , to get the targeted event.
+    editEvent(dayId, eventDetails){
+        this.resetEditOfAllEvents()
+        const dayObj = this.state.data.find((day) => day.id === dayId);
+        const eventObj = dayObj.events.find((event) => event.details === eventDetails);
+        eventObj.edit = true;
+    },
+    resetEditOfAllEvents(){
+        this.state.data.map((dayObj) =>{
+            dayObj.events.map(event =>{
+                event.edit = false;
+            })
+        })
     }
 }
